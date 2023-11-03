b10595639c42e379d4132d5ae6d95dbedc23c74b
Bright11 <chikanwazuo@gmail.com>
2023-11-03 05:49:37 +0000

    media query

--- js/myjs.js
+++ js/myjs.js
@@ -4,3 +4,19 @@ const categorysidebar=document.querySelector(".category-items")
 function sidebarclick(){
     categorysidebar.classList.toggle('category-items-opend')
 }
+
+
+// searchbardiv
+const search=document.querySelector(".searchbardiv")
+const closeformbtn=document.querySelector(".closeform-div")
+
+function opensearchform(){
+    search.classList.toggle("searchitem")
+    closeformbtn.classList.toggle("searchitem")
+}
+
+
+function closeform(){
+    search.classList.remove("searchitem")
+    closeformbtn.classList.remove("searchitem")
+}
